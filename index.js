47cc67add1094590f3e23824e70bbb0a36267849
marve1icious <34216187+D3ord3NidAm@users.noreply.github.com>
2022-09-21 00:46:44 -0500

    rpc upload

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+import 'dotenv/config';
+import { Client } from 'discord-rpc';
+const { clientId } = process.env;
+const rpc = new Client({ transport: 'ipc' });
+const startTimestamp = new Date();
+
+async function setActivity() {
+    if (!rpc) {
+        return;
+    }
+
+    rpc.setActivity({
+        details: `sern handler`,
+        state: 'INSTALL CLI TO START',
+        startTimestamp,
+        largeImageKey: 'sern',
+        largeImageText: 'sern',
+        smallImageKey: 'rpc-verify',
+        smallImageText: 'rpc-verify',
+        instance: false,
+        buttons: [
+            {
+                label: `sern handler (v 1.0.0)`,
+                url: `https://sern-handler.js.org/`
+            },
+            {
+                label: `Discord Server`,
+                url: `https://discord.gg/DwbF5H5JgQ`
+            }
+        ]
+    });
+}
+
+rpc.on('ready', () => {
+    setActivity();
+    console.log('ready')
+    setInterval(() => {
+        setActivity();
+    }, 15e3);
+});
+
+rpc.login({ clientId }).catch(console.error);
+
